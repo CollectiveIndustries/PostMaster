29e88673e1cfcb1890d6eb3034fb52e776cd5b83
Andrew Malone <admmorketh@gmail.com>
2024-12-21 20:26:52 -0700

    fixes return value of extract method

--- lib/helper.py
+++ lib/helper.py
@@ -1,16 +1,16 @@
 from .config import config
 
-def extract_email_data(email):
+def extract_email_data(email: tuple):
     """
     Extracts data from the email based on configuration settings.
     """
     data = []
     if config.USE_SUBJECT:
-        data.append(email.subject)
+        data.append(email[0])
     if config.USE_SENDER:
-        data.append(email.sender)
+        data.append(email[1])
     if config.USE_RECIPIENT:
-        data.append(email.recipient)
+        data.append(email[2])
     if config.USE_BODY:
-        data.append(email.body)
+        data.append(email[3])
     return " ".join(data)
